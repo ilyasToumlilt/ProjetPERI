9c5a562fcc0942014c949eabe9e1dbc70719d673
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2015-05-23 17:22:39 +0200

    Random server

--- web/ajax/brightnessAjax.php
+++ web/ajax/brightnessAjax.php
@@ -4,23 +4,7 @@ define('LIGHT_FIFO', "/tmp/lightpipe");
 
 $ret = "";
 
-/**
- * @todo je ne dois pas créer le pipe, mais tant qu'il n'existe pas la section doit être inactive
- */
-if(!file_exists(LIGHT_FIFO)){
-   $ret = "ERROR 020: Daemon isn't lunched yet";
-   goto end_label;
-}
-
-$pipe = fopen(LIGHT_FIFO, "r");
-if( !$pipe ){
-   $ret = "ERROR 021: Cannot open LIGHT_FIFO pipe";
-   goto end_label;
-}
-
-$ret = ord(fread($pipe, 2));
-
-fclose($pipe);
+$ret = rand(0, 100);
 
 end_label:
    header("Content-type: text/plain;charset=utf-8");
--- web/ajax/temperatureAjax.php
+++ web/ajax/temperatureAjax.php
@@ -4,23 +4,7 @@ define('TEMP_FIFO', "/tmp/temppipe");
 
 $ret = "";
 
-/**
- * @todo je ne dois pas créer le pipe, mais tant qu'il n'existe pas la section doit être inactive
- */
-if(!file_exists(TEMP_FIFO)){
-   $ret = "ERROR 030: Daemon isn't lunched yet";
-   goto end_label;
-}
-
-$pipe = fopen(TEMP_FIFO, "r");
-if( !$pipe ){
-   $ret = "ERROR 031: Cannot open TEMP_FIFO pipe";
-   goto end_label;
-}
-
-$ret = ord(fread($pipe, 2));
-
-fclose($pipe);
+$ret = rand(15, 25);
 
 end_label:
    header("Content-type: text/plain;charset=utf-8");
